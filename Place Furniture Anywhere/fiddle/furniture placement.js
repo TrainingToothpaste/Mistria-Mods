73b7e13f8dd548eded32de580e4f8d8213d7a8ae
TrainingToothpaste <michelle.tesv@gmail.com>
2026-06-09 12:29:04 -0400

    initial mods

---
+++ Place Furniture Anywhere/fiddle/furniture placement.js
@@ -0,0 +1,51 @@
+{
+    "object_prototypes": {
+        "furniture": {
+            "default": {
+                "size": [
+                    2,
+                    2
+                ],
+                "depth_offset": 0,
+                "mirror_west": true,
+                "depth_to_floor": false,
+                "interaction_turn_on": false,
+                "interaction_chest": false,
+                "is_journal": false,
+                "is_bed": false,
+                "window_tiles": false,
+                "animal_toy": false,
+                "child_grid": false,
+                "child_grid_offset": [
+                    0,
+                    0
+                ],
+                "ari_craft_offset": [
+                    0,
+                    0
+                ],
+                "fence": false,
+                "rug": false,
+                "pet_bed": false,
+                "pet_dish": false,
+                "destructable": true,
+                "bird_perch": false,
+                "is_date_photo": false,
+                "footstep": false,
+                "placeable_locations": [
+                    "farm",
+                    "player_home",
+                    "player_home_west",
+                    "player_home_east",
+                    "player_home_upper_central",
+                    "player_home_upper_west",
+                    "player_home_upper_east",
+                    "large_greenhouse",
+                    "small_greenhouse"
+                ],
+                "on_twos_only": false,
+                "check_pick": false,
+                "can_be_child": true
+            }
+        }
+    }}
